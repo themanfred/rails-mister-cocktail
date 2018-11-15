447314e0a0ccb2b6a7b5432badd84fa1b8d53144
Thomas Freund <tfreundc@gmail.com>
2018-11-15 16:55:13 +0200

    gemfile changer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Cocktail.destroy_all
+Ingredient.destroy_all
 
 
 
@@ -20,3 +19,12 @@ Dose.create(description: '1.5 oz vodka, 0.5 oz peach schnapps, 1.5 oz pineapple 
 Dose.create(description: '2.5 oz cognac, 0.5 oz Grand Marnier, 0.5 oz lemon juice')
 Dose.create(description: '1 oz gin, 1 oz bitters, 1 oz vermouth')
 Dose.create(description: '2 oz vodka, 3 oz ginger beer, 0.5 lime juice')
+
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+cocktails = open(url).read
+ingredients = JSON.parse(cocktails)
+ingredients['drinks'].each do |ingredient|
+  print 'created one ingredient'
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
